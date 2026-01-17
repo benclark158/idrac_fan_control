209999dc8600b372c033d93f915685f92a60184e
benclark158 <benclark.btc@gmail.com>
2026-01-17 23:49:36 +0000

    General improvements to performance and execution

--- control.py
+++ control.py
@@ -74,6 +74,13 @@ class Ipmi:
         self.send_ipmi_command('raw', '0x30', '0x30', '0x01', '0x01')
         print('Enabled Automatic Fans')
 
+    def get_cpu_util(self) -> int:
+        result, _, _ = self.send_ipmi_command('sdr', 'get', 'CPU Usage', '0x01', '0x01')
+        reading: str = [ line for line in result.split('\n') if 'Sensor Reading' in line][0]
+        return int(reading.strip().split(':', 1)[1].strip().split(' ')[0])
+
+
+
 class Line:
     slope: float
     intercept: float
@@ -137,8 +144,9 @@ class FanMonitor:
 
         return Line(slope=slope, intercept=intercept)
 
-    def print_table_row(self, temps: dict[str, int], target_speed: int, include_headings: bool = False):
+    def print_table_row(self, temps: dict[str, int], target_speed: int, st: float = 0, include_headings: bool = False):
         t = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cpu_usage = 'na' # self.__ipmi.get_cpu_util()
 
         temp_cols = [ (key.title(), len(key) + 2, str(value)) for key, value in temps.items() ]
         
@@ -147,7 +155,13 @@ class FanMonitor:
             *temp_cols
         ]
 
-        columns.append(("Fan Speed", 10, f"{target_speed}%"))
+        runtime_ms: float = (time.time() - st) * 1000
+
+        columns.extend([
+            ("Fan Speed", 10, f"{target_speed}%"),
+            ("CPU Util", 10, f"{cpu_usage}%"),
+            ("Loop Runtime", 14, f"{int(runtime_ms)} ms")
+        ])
         
         left_pad = 2
         if include_headings:
@@ -157,24 +171,41 @@ class FanMonitor:
         print('|'.join([ f'{" " * left_pad}{value}{ " " * (length - len(value)) }' for _, length, value in columns]))
 
     def run(self):
-        line = self.__calculate_function()
+        try:
+            self.__unsafe_run()
+        except Exception as _:
+            self.cleanup()()
+
+    def __unsafe_run(self):
+        start_time = time.time()
         self.__ipmi.disable_auto_fans()
-        self.print_table_row(temps=self.__ipmi.get_temps(), target_speed='na', include_headings=True)
+        self.print_table_row(temps=self.__ipmi.get_temps(), target_speed='na', include_headings=True, st=start_time)
+
+        line = self.__calculate_function()
         while True:
-            temp = self.__ipmi.get_temps()
-            cpu_temp = [ temp for key, temp in temp.items() if key.lower().startswith('cpu')]
-            max_cpu_temp = max(cpu_temp)
-            target_fan_speed = int(line.calculate(max_cpu_temp))
+            try:
+                self.__loop(line=line)
+            except KeyboardInterrupt as e:
+                raise e
+            except Exception as _:
+                print('Error when controlling fan speed!')
 
-            if target_fan_speed < self.start_fan:
-                target_fan_speed = self.start_fan
-            if target_fan_speed > self.end_fan:
-                target_fan_speed = self.end_fan
+    def __loop(self, line: Line):
+        loop_start_time = time.time()
+        temp = self.__ipmi.get_temps()
+        cpu_temp = [ temp for key, temp in temp.items() if key.lower().startswith('cpu')]
+        max_cpu_temp = max(cpu_temp)
+        target_fan_speed = int(line.calculate(max_cpu_temp))
 
-            self.__ipmi.set_fan_speed(target_fan_speed)
-            self.print_table_row(temps=temp, target_speed=target_fan_speed)
+        if target_fan_speed < self.start_fan:
+            target_fan_speed = self.start_fan
+        if target_fan_speed > self.end_fan:
+            target_fan_speed = self.end_fan
+
+        self.print_table_row(temps=temp, target_speed=target_fan_speed, st=loop_start_time)
+        self.__ipmi.set_fan_speed(target_fan_speed)
             
-            time.sleep(self.interval)
+        time.sleep(self.interval - (time.time() - loop_start_time))
 
 if __name__ == '__main__':
     fan_monitor: FanMonitor = FanMonitor()
