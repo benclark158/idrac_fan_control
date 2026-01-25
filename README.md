e924e1d8eea0fd052a100b5e679bed539d1df8bb
benclark158 <benclark.btc@gmail.com>
2026-01-25 15:44:38 +0000

    Improving a few bits and adding docs

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # idrac_fan_control
-This python/docker library is used for iDRAC servers to control their fan speed based on CPU Temperatures
+This python/docker library is used for iDRAC and IPMI servers to control their fan speed based on CPU Temperatures
+
+## Fan Speed Calculation
+
+For this package you can set a start temperature (`START_TEMP`) and end temperature (`END_TEMP`). When the system is below the `START_TEMP` then the `START_FAN` speed is applied to all fans. If you temperature is equal or above the `END_TEMP` then the `END_FAN` speed is applied to all fans.
+
+Between these two points a linear gradient is used. The below graph shows how the fan speed changes with CPU temperature. The `x` axis is the temperature of the CPU and the `y` axis is the fan speed.
+
+![Fan Speed vs Temp Graph](assets/graph.png)
+
+You should optimise these values in line with your CPU and other hardware specifications.
+
+## Docker Compose
+
+```yaml
+services:
+  idrac-fan-control:
+    image: ghcr.io/benclark158/idrac_fan_control:latest
+    container_name: idrac_fan_control
+    restart: unless-stopped
+    environment:
+      - IPMI_HOST=<192.168.0.120> # also accepts DNS names
+      - IPMI_USER=<root>
+      - IPMI_PWD=<calvin>
+      - START_TEMP=<40> # CPU temp in C
+      - START_FAN=<0> # Fan speed in %
+      - END_TEMP=<65> # CPU Temp in C
+      - END_FAN=<60> # Fan speed in %
+      - INTERVAL=<30> # default interval between checks in seconds
+      - GET_CPU_UTIL=<true|false> # Should the CPU utilisation be output in the termal - this slows the loop - default False
+```
+
+### Setup
+
+You need to enable IPMI over the network for this container to work correctly.
+
+## Support
+
+This extension has only been tested on Dell servers (specifically Dell R730xd), the functionality may be different for other brands or newer versions of IPMI/iDRAC.
--- control.py
+++ control.py
@@ -12,7 +12,7 @@ class Ipmi:
     __password: str
 
     def __init__(self, host: str | None = None, username: str | None = None, password: str | None = None):
-        self.__host = host or '192.168.0.120'
+        self.__host = host or 'idrac-37bjzg2'
         self.__username = username or 'root'
         self.__password = password or 'calvin'
 
@@ -24,7 +24,7 @@ class Ipmi:
             '-U', f'{self.__username}',
             '-P', f'{self.__password}',
             *args
-        ], capture_output=True, text=True)
+        ], capture_output=True, text=True, timeout=30.0)
 
         return result.stdout, result.stderr, ' '.join(result.args)
 
@@ -82,6 +82,15 @@ class Ipmi:
 
 
 class Line:
+    @classmethod
+    def fit(self, p1: tuple[float, float], p2: tuple[float, float]) -> "Line":
+        pass
+
+    def calculate(self, x: float) -> float:
+        pass
+
+
+class Linear(Line):
     slope: float
     intercept: float
 
@@ -89,8 +98,22 @@ class Line:
         self.slope = slope
         self.intercept = intercept
         
-    def calculate(self, temp: int | float) -> float:
-        return (temp * self.slope) + self.intercept
+    def calculate(self, x: int | float) -> float:
+        return (x * self.slope) + self.intercept
+
+    @classmethod
+    def fit(cls, p1: tuple[float, float], p2: tuple[float, float]) -> "Linear":
+        # y = mx + c
+        x1, y1 = p1
+        x2, y2 = p2
+
+        slope = float(y2 - y1) / float(x2 - x1)
+        intercept = y1 - slope * x1
+
+        return Linear(slope=slope, intercept=intercept)
+    
+    def __str__(self):
+        return f'y = {self.slope}*x + {self.intercept}'
     
 class FanMonitor:
     __ipmi: Ipmi
@@ -102,9 +125,11 @@ class FanMonitor:
 
     interval = 30
 
+    get_cpu_util: bool = False
+
     def __init__(self):
         # Load from env
-        host = os.environ.get('IPMI_IP')
+        host = os.environ.get('IPMI_HOST')
         username = os.environ.get('IPMI_USER')
         password = os.environ.get('IPMI_PWD')
 
@@ -113,6 +138,8 @@ class FanMonitor:
         self.end_temp = os.environ.get('END_TEMP') or self.end_temp
         self.end_fan = os.environ.get('END_FAN') or self.end_fan
         self.interval = os.environ.get('INTERVAL') or self.interval
+
+        self.get_cpu_util = str(os.environ.get('GET_CPU_UTIL', False)) in ['t', '1', 'true', 'yes']
 
         # IPMI
         self.__ipmi = Ipmi(host=host, username=username, password=password)
@@ -138,15 +165,11 @@ class FanMonitor:
         # end_fan start_fan-100
         self.end_fan = min(100, max(self.start_fan, self.end_fan))
 
-        # y = mx + c
-        slope = float(self.end_fan - self.start_fan) / float(self.end_temp - self.start_temp)
-        intercept = self.start_fan - slope * self.start_temp
-
-        return Line(slope=slope, intercept=intercept)
+        return Linear.fit((self.start_temp, self.start_fan), (self.end_temp, self.end_fan))
 
     def print_table_row(self, temps: dict[str, int], target_speed: int, st: float = 0, include_headings: bool = False):
         t = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        cpu_usage = 'na' # self.__ipmi.get_cpu_util()
+        cpu_usage = self.__ipmi.get_cpu_util() if self.get_cpu_util else '??'
 
         temp_cols = [ (key.title(), len(key) + 2, str(value)) for key, value in temps.items() ]
         
@@ -173,22 +196,33 @@ class FanMonitor:
     def run(self):
         try:
             self.__unsafe_run()
-        except Exception as _:
+        except KeyboardInterrupt as e:
             self.cleanup()()
+        except Exception as e:
+            self.cleanup()()
+            raise e
 
     def __unsafe_run(self):
         start_time = time.time()
         self.__ipmi.disable_auto_fans()
         self.print_table_row(temps=self.__ipmi.get_temps(), target_speed='na', include_headings=True, st=start_time)
+        error_count = 0
 
         line = self.__calculate_function()
         while True:
             try:
                 self.__loop(line=line)
+                error_count = 0
             except KeyboardInterrupt as e:
                 raise e
-            except Exception as _:
+            except Exception as e:
                 print('Error when controlling fan speed!')
+
+                # Exit the loop and cleanup if there are more than 10 errors in a row
+                # When this is in docker it should auto heal.
+                if error_count >= 10:
+                    raise e
+                error_count += 1
 
     def __loop(self, line: Line):
         loop_start_time = time.time()
